b2b06be408ba3061f149ffa8dd3e7333f4c54ec1
Sandor2889 <sandor2889@naver.com>
2022-10-25 03:27:24 +0900

    Add programmers Level2 - 올바른 괄호

---
+++ C++/2022-10-24-Level2_11.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include<string>
+
+using namespace std;
+
+bool solution(string s)
+{
+    bool answer = true;
+    int open = 0;
+
+    // '(' 면 open을 증가하고 ')' 면 open을 감소시킨다.
+    // open이 0이 되어야 올바르게 짝지어진 것이다.
+    for (int i = 0; i < s.size(); i++)
+    {
+        if (s[i] == ')')    // ')' 이고 '(' 없을 경우 false 반환하고 종료
+        {
+            if (open == 0) { return false; }
+            else { open -= 1; }
+        }
+        else  // '(' 이면 수량 증가 
+        {
+            open++;
+        }
+    }
+
+    // open 괄호에 대한 짝(close)이 맞춰지지 않으면 false
+    if (open != 0) { answer = false; }
+
+    return answer;
+}
+int main()
+{
+    string s = { "(())()" };
+
+    bool answer = solution(s);
+    cout << answer;
+
+    return 0;
+}
+
