7b1fa3bca23ecd54560020a1b295add6b7e32163
Sandor2889 <sandor2889@naver.com>
2022-11-18 19:29:47 +0900

    Add programmers Level2 - 모음사전

---
+++ C++/2022-11-10-Level2_46.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int answer, cnt;
+
+// 완전탐색
+void dfs(string curr, string target, char alpha[])
+{
+    if (curr == target) { answer = cnt; }   // 타겟과 같아지면 answer에 대입
+    if (curr.length() > 5) { return; }
+    
+    ++cnt;
+    for (int i = 0; i < 5; ++i)
+    {
+        dfs(curr + alpha[i], target, alpha);
+    }
+}
+
+int solution(string word)
+{
+    char alpha[5] = { 'A', 'E', 'I', 'O', 'U' };
+    dfs("", word, alpha);
+    return answer;
+}
+
+int main()
+{
+    string word = "EIO";
+    answer = solution(word);
+    cout << answer;
+
+    return 0;
+}
